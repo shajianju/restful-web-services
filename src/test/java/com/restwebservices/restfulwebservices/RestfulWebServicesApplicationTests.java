bcbfbe6cab2daf12c2a9bcfe74fac90d1101a00e
anjushaji1996 <shajianju1996@gmail.com>
2022-03-17 10:24:16 +0530

    added junit test cases

--- src/test/java/com/restwebservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/restwebservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,13 +1,59 @@
 package com.restwebservices.restfulwebservices;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.restwebservices.restfulwebservices.models.Account;
+import com.restwebservices.restfulwebservices.models.Employee;
+import com.restwebservices.restfulwebservices.repository.AccountRepo;
+import com.restwebservices.restfulwebservices.repository.EmployeeRepo;
+
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class RestfulWebServicesApplicationTests {
+	
+	@Autowired
+	AccountRepo accountRepo;
+	
+	@Autowired
+	EmployeeRepo employeeRepo;
+
 
 	@Test
-	void contextLoads() {
+	public void testCreate() {
+		
+		Account acc=new Account();
+		acc.setName("ABC");
+		acc.setEmpList(new ArrayList<>());
+		accountRepo.save(acc);
+		assertNotNull(accountRepo.findAll());
+		
 	}
-
+	
+	@Test
+	public void testAddNewUser() {
+		
+		Employee employee=new Employee("AKHIL", "31/12/1995");
+		
+		employeeRepo.save(employee);
+		
+		assertNotNull(employeeRepo.findAll());
+		
+	}
+	
+	
+	@Test
+	public void testListAll() {
+		assertThat(accountRepo.findAll()).size().isGreaterThan(0);
+		
+	}
 }
